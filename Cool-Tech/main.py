c0bc7c99c71732a7463a63c5c8ccac3ace58d5d4
Spaxly <68564017+Spaxly@users.noreply.github.com>
2021-04-08 10:37:40 -0400

    Update main.py

--- Cool-Tech/main.py
+++ Cool-Tech/main.py
@@ -10,18 +10,6 @@ client.remove_command("help")
 async def on_ready():
     print("I'm Ready!")
 
-@client.command()
-async def load(ctx, extension):
-    client.load_extension(f"cogs.{extension}")
-
-@client.command()
-async def unload(ctx, extension):
-    client.unload_extension(f"cogs.{extension}")
-
-
-for filename in os.listdir("./cogs"):
-    pass 
-
 client.load_extension("cogs.fun")
 client.load_extension("cogs.help")
 client.load_extension("cogs.mod")
